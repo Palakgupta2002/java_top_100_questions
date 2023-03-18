cf415634c98f3cd6199d5626e38b0ac16e83a38d
Palakgupta2002 <pg948303@gmail.com>
2023-03-19 01:16:16 +0530

    2 problem

--- Posneg.java
+++ Posneg.java
@@ -2,7 +2,34 @@ import java.util.*;
 class posneg{
     public static void main(String args[]) {
 Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+int number=sc.nextInt();
+oddeven(number);
+ 
+    }
+    public static void oddeven(int number) {
+        if(number%2==0){
+            System.out.println("the enter number is even");
+        }
+        else{
+            System.out.println("the enter number is odd");
+        }
+
 
         
+    }
+    public static void posnegnum( int number){
+        if(number==0){
+            System.out.println("the number is zero");
+        }
+        else if(number>0){
+            System.out.println("the enter number is positive");
+        }
+        else{
+            System.out.println("the enter number is negative");
+        }
+        
+               
+
     }
 }
