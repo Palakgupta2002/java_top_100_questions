1dd11a435b0d67bba6e8d3f3b3deff34bd3c9098
Palakgupta2002 <pg948303@gmail.com>
2023-03-29 23:40:38 +0530

    Prepinsta Problem List second

--- prepinsta2.java
+++ prepinsta2.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import static java.lang.Math.*;
 
 public class prepinsta2 {
     public static void main(String[] args) {
@@ -382,6 +383,30 @@ public class prepinsta2 {
             System.out.println("Number in words is a "+ numbertowords(number));
            }
         }
+       //Finding Roots of a quadratic equation in Java
+       public static void quadraticequation(int a,int b,int c){
+        if(a==0){
+            System.out.println("invalid");
+            return;
+        }
+        int d = b * b - 4 * a * c;
+        double sqrt_val = sqrt(abs(d));
+        if (d > 0) {
+            System.out.println("Roots are real and different");
+            System.out.println((double)(-b + sqrt_val) / (2 * a) + "\n"+ (double)(-b - sqrt_val) / (2 * a));
+        }
+        else if (d == 0) {
+            System.out.println("Roots are real and same ");
+            System.out.println(-(double)b / (2 * a) + "\n" + -(double)b / (2 * a));
+        }
+        else // d < 0
+        {
+            System.out.println("Roots are complex");
+ 
+            System.out.println(-(double)b / (2 * a) + " + i" + sqrt_val + "\n" + -(double)b / (2 * a) + " - i" + sqrt_val);
+        }
+    }
+    //
 
         
 
