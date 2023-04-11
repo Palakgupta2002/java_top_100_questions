13f03b75ae01e94381028d88d114a24e662e42f8
Palakgupta2002 <pg948303@gmail.com>
2023-04-11 16:43:12 +0530

    Prepinsta Problem list 4

--- Prepinsta4.java
+++ Prepinsta4.java
@@ -14,7 +14,8 @@ public class Prepinsta4 {
         //    largestsmallest(Array, n);
         // secondsmallestnumber(Array, n);
         // arraysum(Array, n, 0);
-        System.out.print( arraysumrecursion(Array, n, 0,0));
+        // System.out.print( arraysumrecursion(Array, n, 0,0));
+        slidingreversearray(Array, n);
         }
         //Java Program for finding the largest element of the array
         public static int getmax(int [] array,int n){
@@ -58,7 +59,7 @@ for(int i=0;i<n;i++){
 System.out.println(sum); 
 
 }
-
+//print array sum by recurssion
 
 public static int arraysumrecursion(int arr[],int n,int sum,int pos){
     if(pos==n){
@@ -66,6 +67,33 @@ public static int arraysumrecursion(int arr[],int n,int sum,int pos){
     }
     return arraysumrecursion(arr,n,sum+arr[pos],pos+1);
 
+}
+//print reverse pattern of array
+//there is three way to do this
+//1 approach is :- brute forse to declare one another array and do this
+//2 approch is:- secondly i traverse or print it in reverse form 
+//3 approch is= i do this one by one to take two iterator mean by sliding window
+public static void slidingreversearray(int array[],int n){
+    int i=array.length-1;
+    int j=0;
+    int temp;
+    for(int p=0;p<n;p++){
+        if(i==j){
+            for(int k=0;k<n;k++){
+                System.out.print(array[k]+" ");
+            }
+            return;
+        }
+        else{
+        temp=array[j];
+        array[j]=array[i];
+        array[i]=temp;
+        j++;
+        i--;
+        }
+    }
+       
+    
 }
 
 }
