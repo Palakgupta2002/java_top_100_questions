9ad6b8bf89f105bcec8cf4b41bc01d28b7556ebf
Palakgupta2002 <pg948303@gmail.com>
2023-07-11 12:36:10 +0530

    Prepinsta6

--- prepinsta6.java
+++ prepinsta6.java
@@ -10,7 +10,7 @@ public class prepinsta6 {
         // int ary1[]= {8, 4, 2, 1};
         // int arr[]={7, 1, 5, 3, 6, 4};
         // int sum=8;
-        int[] arr1 = {4, 2, -3, 1, 6};
+        // int[] arr1 = {4, 2, -3, 1, 6};
         // int ary2[]={0,2,4,6,8,9};
         // int k=3;
         // union(ary1, ary);
@@ -25,7 +25,8 @@ public class prepinsta6 {
         // countinvrsion(ary1);
         // buysell(arr);
         // sumequal(arr, sum);
-System.out.print(findsunarraysum(arr1));
+// System.out.print(findsunarraysum(arr1));
+largenofact(22);
 
        
 
@@ -340,6 +341,49 @@ public static void Kadanesalgo(int arr[]){
                 return false;
         
     }
+    //Factorial of a Large Number in Java
+    public static void largenofact(int n){
+        int res[] = new int[500];
+ 
+        // Initialize result
+        res[0] = 1;
+        int res_size = 1;
+ 
+        // Apply simple factorial formula
+        // n! = 1 * 2 * 3 * 4...*n
+        for (int x = 2; x <= n; x++)
+            res_size = multiply(x, res, res_size);
 
+        System.out.println("Factorial of given number is ");
+        for (int i = res_size - 1; i >= 0; i--)
+            System.out.print(res[i]);
+
+    }
+    //multiply , this is a helping function
+    public static int multiply(int x,int res[],int res_size){
+        int carry = 0; // Initialize carry
+
+        // One by one multiply n with individual
+        // digits of res[]
+        for (int i = 0; i < res_size; i++)
+        {
+            int prod = res[i] * x + carry;
+            res[i] = prod % 10; // Store last digit of
+                                // 'prod' in res[]
+            carry = prod/10; // Put rest in carry
+        }
+
+        // Put carry in res and increase result size
+        while (carry!=0)
+        {
+            res[res_size] = carry % 10;
+            carry = carry / 10;
+            res_size++;
+        }
+        return res_size;
+    }
+
+    // Driver program
+   
 
 }
