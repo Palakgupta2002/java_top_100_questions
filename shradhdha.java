417dcced52728ae974d5d7200f19338ef7061fda
Palakgupta2002 <pg948303@gmail.com>
2023-07-26 21:21:45 +0530

    Process

--- shradhdha.java
+++ shradhdha.java
@@ -27,3 +27,23 @@ class shradhdha{
     }
 }
 }
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int si = 0;
+        int ei = nums.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2; 
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+
+       
+        return si;
+    }
+}
